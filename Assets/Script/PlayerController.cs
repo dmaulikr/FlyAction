583522ef1bf909e675b2c439f062623c98c996d0
kyn320@naver.com <kyn320@naver.com>
2017-07-19 21:07:23 +0900

    초기 커밋 리소스 추가

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
+public class PlayerController : MonoBehaviour
+{
+
+    Transform tr;
+    Rigidbody ri;
+
+    [SerializeField]
+    float moveSpeed = 5f, rotateSpeed =5f,  curveMax = 80f;
+
+    [SerializeField]
+    float h, v;
+
+    void Awake()
+    {
+        tr = GetComponent<Transform>();
+        ri = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate() {
+
+        tr.Rotate(Vector3.left * v * curveMax);
+        tr.Rotate(Vector3.up * h*curveMax);
+        //tr.rotation = Quaternion.Slerp(tr.rotation, Quaternion.Euler(tr.rotation.x + v * -curveMax, tr.rotation.y + h * curveMax, 0),Time.deltaTime * rotateSpeed);
+        ri.AddForce(tr.forward * moveSpeed);
+    }
+
+
+}
